c7b5c07dba71fe2e5b90e40a8102ce4afed2cd1f
mohitkundkar598 <101802286+mohitkundkar598@users.noreply.github.com>
2022-04-11 13:41:40 +0530

    Add files via upload

---
+++ mohit.c
@@ -0,0 +1,13 @@
+/*addition of two numbers*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+
+{
+	int a,b,c;
+	printf("enter a,b");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("the result is=%d",c);
+	getch();
+}
