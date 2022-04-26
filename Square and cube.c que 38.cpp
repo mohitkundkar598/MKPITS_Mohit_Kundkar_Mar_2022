5c171f8e4a03c5a50b8876873847aba8aa93a918
mohitkundkar598 <101802286+mohitkundkar598@users.noreply.github.com>
2022-04-26 22:25:20 +0530

    Add files via upload

---
+++ Square and cube.c que 38.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main() 
+{
+	int a, i, j = 1, x = 0;
+	printf("Input number of lines: ");
+	scanf("%d", &a);
+	
+    for(i = 1; i <= a; i++) {
+		printf("%d %d %d\n", i, i*i, i*i*i);
+	  }
+	
+	return 0;
+}
