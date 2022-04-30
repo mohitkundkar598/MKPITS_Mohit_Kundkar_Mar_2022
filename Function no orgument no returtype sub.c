961231534cfba986993aa11d93d7a84dabd6fb92
mohitkundkar598 <101802286+mohitkundkar598@users.noreply.github.com>
2022-04-30 15:45:00 +0530

    Add files via upload

---
+++ Function no orgument no returtype sub.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void sub();
+void main()
+{
+	sub();
+}
+void sub()
+{
+int n1,n2,res;
+printf("enter any two num:");
+scanf("%d %d" ,&n1, &n2);
+res=n1-n2;
+printf("sub=%d",res);
+getch();
+}
