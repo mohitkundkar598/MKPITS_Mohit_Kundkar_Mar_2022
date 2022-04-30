0de556f10b0d1b82225648287aec3a1f789c795d
mohitkundkar598 <101802286+mohitkundkar598@users.noreply.github.com>
2022-04-30 15:44:05 +0530

    Add files via upload

---
+++ Function no orgument no returntype div.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void div();
+void main()
+{
+	div();
+}
+void div()
+{
+int n1,n2,res;
+printf("enter any two num:");
+scanf("%d %d" ,&n1, &n2);
+res=n1/n2;
+printf("div=%d",res);
+getch();
+}
