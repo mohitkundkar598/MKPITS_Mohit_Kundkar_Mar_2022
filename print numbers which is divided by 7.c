abdc9cb5a664b9772bb7ccb0999dec88da0a23b1
mohitkundkar598 <101802286+mohitkundkar598@users.noreply.github.com>
2022-05-05 09:17:16 +0530

    Add files via upload

---
+++ print numbers which is divided by 7.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int n1,n2,i,temp;
+	printf("enter first number\n");
+	scanf("%d",&n1);
+	printf("enter second number\n");
+	scanf("%d",&n2);
+	printf("number which is divided by 7 \n");
+	if(n1>n2)
+	{
+		temp=n1;
+		n1=n2;
+		n2=temp;
+	}
+	for(i=n1;i<=n2;i++)
+	{
+		if(i%7==2||i%7==3)
+		{
+		printf("%d ",i);
+		}
+	}
+}
+
