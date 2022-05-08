bf2d9d5ebc76f11324e13104a23209b5103df47f
mohitkundkar598 <101802286+mohitkundkar598@users.noreply.github.com>
2022-05-08 12:33:17 +0530

    Add files via upload

---
+++ que no.41.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main() 
+{
+	float S = 0;
+	int i;
+	for(i=1; i<=50; i++)
+	{
+		S += (float)1/i;
+    }
+    printf("Value of S: %.2f\n", S);
+
+	return 0;
+}
